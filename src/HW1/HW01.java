9eb87383538cfe458698b4827d11a40edd584512
Eric chen <chenpo0407@gmail.com>
2023-09-04 16:39:27 +0800

    push hw1 to github

---
+++ src/HW1/HW01.java
@@ -0,0 +1,42 @@
+package HW1;
+
+public class HW01 {
+	public static void main(String[] args) {
+		/*
+		int x=12; int y=6;
+		System.out.println(x+y);
+		System.out.println(x*y);
+		*/
+		//=========================
+		/*
+		int egg=200; int box=12;
+			System.out.println(egg/box);  //16打
+			System.out.println(egg%box); //剩八顆
+		*/
+		
+//		int sec=256559; int day=60*60*24;
+//		int hour=60*60; int min=60;
+//		
+//		System.out.println(sec/day);
+//		System.out.println(sec%day/hour);
+//		System.out.println(sec%hour/min);
+//		System.out.println(sec%min);
+//		System.out.println(59+15*60+23*60*60+2*60*60*24);
+//		
+//		double pi=3.1415;  int r=5;
+//		System.out.println(Math.pow(r,2)*pi);
+//		System.out.println(10*pi);
+//		
+//		int money=1500000; double r=0.02;int n=10;
+//		System.out.println(money*Math.pow(1.02,n));
+//		
+//		System.out.println(5+5);     10 數字產生
+//		System.out.println(5+'5');	58一個數字加上一個字元所產生
+//		System.out.println(5+"5");	數字加上字串所產生
+//		
+		
+		
+		
+		}
+
+}
